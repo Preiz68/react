07bd2061d521eb68d074d8d0ea2700137856190c
Akoji Praise <akojiomede75@gmail.com>
2025-03-18 18:04:13 -0700

    update the modals

--- src/Components/EditEmployee.js
+++ src/Components/EditEmployee.js
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -35,7 +37,11 @@ function EditEmployee() {
                     <div className="md:w-2/3">
                     <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full 
                     py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="name" type="text" value="Jane Doe"/>
+                    id="name" type="text" value= {name}
+                    onChange={(e)=>{
+                        setName(e.target.value)
+                    }}
+                    />
                     </div>
                 </div>
                 <div className="md:flex md:items-center mb-6">
@@ -47,7 +53,11 @@ function EditEmployee() {
                     <div className="md:w-2/3">
                     <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full 
                     py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="role" type="text" value="Jane Doe"/>
+                    id="role" type="text" value= {role}
+                    onChange={(e)=>{
+                        setRole(e.target.value)
+                    }}
+                    />
                     </div>
                 </div>
                 </form>
